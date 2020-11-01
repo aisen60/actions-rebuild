27cae1712ad10c90e15186c90b30aef4476ba2af
Aisen60 <caixusheng60@gmail.com>
2020-11-01 20:38:54 +0800

    feat: update index.js

--- index.js
+++ index.js
@@ -6,23 +6,24 @@ const port = 1338;
 app.all("/:token/:owner/:repo/:event_type", (req, res) => {
   console.log(req.params);
   const { token, owner, repo, event_type } = req.params;
-  if (token && owner && repo && event_type) {
-    const octokit = new Octokit({
-      auth: token,
-    });
-    octokit
-      .request("POST /repos/{owner}/{repo}/dispatches", {
-        owner,
-        repo,
-        event_type,
-      })
-      .then((_) => {
-        res.send(`repo actions is rebuild`);
-      })
-      .catch((err) => {
-        res.send(`failed: ${err}`);
-      });
-  }
+  // if (token && owner && repo && event_type) {
+  //   const octokit = new Octokit({
+  //     auth: token,
+  //   });
+  //   octokit
+  //     .request("POST /repos/{owner}/{repo}/dispatches", {
+  //       owner,
+  //       repo,
+  //       event_type,
+  //     })
+  //     .then((_) => {
+  //       res.send(`'${repo}' actions is rebuild`);
+  //     })
+  //     .catch((err) => {
+  //       res.send(`failed: ${err}`);
+  //     });
+  // }
+  res.send(req.params);
 });
 
 app.listen(port, () => {
