ea4702da77b05dd38abf096b8201ef673025354f
Aisen60 <caixusheng60@gmail.com>
2020-11-01 20:42:56 +0800

    feat: update index.js

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ const { Octokit } = require("@octokit/rest");
 const app = express();
 const port = 1338;
 
-app.all("/:token/:owner/:repo/:event_type", (req, res) => {
-  console.log(req.params);
-  const { token, owner, repo, event_type } = req.params;
-  // if (token && owner && repo && event_type) {
-  //   const octokit = new Octokit({
-  //     auth: token,
-  //   });
-  //   octokit
-  //     .request("POST /repos/{owner}/{repo}/dispatches", {
-  //       owner,
-  //       repo,
-  //       event_type,
-  //     })
-  //     .then((_) => {
-  //       res.send(`'${repo}' actions is rebuild`);
-  //     })
-  //     .catch((err) => {
-  //       res.send(`failed: ${err}`);
-  //     });
-  // }
-  res.send(req.params);
+// app.all("/:token/:owner/:repo/:event_type", (req, res) => {
+//   console.log(req.params);
+//   const { token, owner, repo, event_type } = req.params;
+//   // if (token && owner && repo && event_type) {
+//   //   const octokit = new Octokit({
+//   //     auth: token,
+//   //   });
+//   //   octokit
+//   //     .request("POST /repos/{owner}/{repo}/dispatches", {
+//   //       owner,
+//   //       repo,
+//   //       event_type,
+//   //     })
+//   //     .then((_) => {
+//   //       res.send(`'${repo}' actions is rebuild`);
+//   //     })
+//   //     .catch((err) => {
+//   //       res.send(`failed: ${err}`);
+//   //     });
+//   // }
+//   res.send(req.params);
+// });
+
+app.all("/", (req, res) => {
+  res.send("123");
 });
 
 app.listen(port, () => {
